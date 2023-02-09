f44b3bb8b2c7a42516481f34ad03f62fa2804dda
Priyanka Senthilkumar <priyanka.s@domain.rage-india.com>
2023-02-09 22:07:13 +0530

    Updates

---
+++ precache-manifest.1f8e477142bbbf2099a03065a1050797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc28b490ecdef8228f020d73655108b",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "436210dec40866acdda9",
+    "url": "/react-movie/static/css/2.e92c2c3e.chunk.css"
+  },
+  {
+    "revision": "436210dec40866acdda9",
+    "url": "/react-movie/static/js/2.798ea1bb.chunk.js"
+  },
+  {
+    "revision": "891ac9059e8de67d78af",
+    "url": "/react-movie/static/js/main.df83d4b7.chunk.js"
+  },
+  {
+    "revision": "5de7cd1febd31cdee4f7",
+    "url": "/react-movie/static/js/runtime~main.b4244ab5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-movie/static/media/logo.5d5d9eef.svg"
+  }
+]);
